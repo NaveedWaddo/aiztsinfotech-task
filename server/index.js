d918369ce55e7daec645365f2693ce6dbbaf1355
Naveed Waddo <naveedwaddo1123@gmail.com>
2024-12-01 11:58:56 +0530

    MongoDB Setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./database/db.js";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  console.log("server is running");
+});
 
 app.listen(5000, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDb();
 });
